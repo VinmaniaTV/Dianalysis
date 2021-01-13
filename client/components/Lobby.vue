bbc140933571946ef860fb56808c724d716b18fe
Tarzan <younes.anouar@efrei.net>
2021-01-13 17:48:13 +0100

    Lobby up date css

--- client/components/Lobby.vue
+++ client/components/Lobby.vue
@@ -1,23 +1,27 @@
 <template>
     <section>
-      <nav>
+      <nav class="">
         <h2>Bon retour #Guest#</h2>
       </nav>
     <div id="section_y">
       <h2 class="title_container">Accueil</h2>
       <div class="sommaire">
-        <router-view>
-        <button class="button_sommaire" onclick="window.location.href = '/#/programme';">Votre Programme</button>
-        <button class="button_sommaire" onclick="window.location.href = '/#/seche';">Consulter notre liste d'aliments</button>
-        <button class="button_sommaire" onclick="window.location.href = '/#/seche';">Nos Conseils</button>
-        <button class="button_sommaire" onclick="window.location.href = '/#/seche';">Votre Historique</button>
+          <router-link class="summary" to="/programme">Programme</router-link>
+          <router-link class="summary" to="/listeAliments">Consulter notre liste d'aliments favoris</router-link>
+          <router-link class="summary" to="/conseils">Nos Conseils</router-link>
+          <router-link class="summary" to="/historique">Votre Historique</router-link>
       </div>
+
     </div>
     </section>
 </template>
 
 <style>
+section{
+  background-color: #2D3F61;
+}
 .title_container{
+  padding-top: 55px;
   background-color: black;
   color: white;
   text-align: center;
@@ -29,6 +33,7 @@
 
 .section_y{
   height: 1vh;
+  text-align: center;
 }
 .sommaire{
   display: flex;
@@ -36,8 +41,23 @@
   align-items: center;
 }
 
-.button_sommaire{
+.summary{
+  color: white;
+  background-color: #4A68A1;
   height: 140px;
-  width: 30em;
+  width: 50em;
+  text-decoration: none;
+  text-align: center;
+  border: solid #5B81C7;
+  border-radius: 5px;
+  padding-top: 35px;
+}
+
+.summary:hover{
+  text-decoration: none;
+  color: white;
+  background: linear-gradient(to right, #2D3F61, #6D9AED, #2D3F61);
+  transition: 2s, 2s;
+  font-size: 40px;
 }
 </style>
--- client/components/programme.vue
+++ client/components/programme.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
       <nav><h2>Bienvenue dans la section Programme</h2></nav>
+      <div></div>
   </section>
 </template>
